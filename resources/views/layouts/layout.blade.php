cf6cf6fc2a683f20f5bef6cd3e0b72d7f1494572
Yassineafaila <yassineafaila55@gmail.com>
2024-01-16 11:31:41 +0000

    add the cdn for jquery

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,37 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- ---cdn-font-awesome-- -->
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-      integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    {{-- ---jquery-cdn--- --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+        integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     {{-- ---alpine-js --}}
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link rel="stylesheet" href="css/app.css" />
     {{-- --tailwind--- --}}
-        @vite('resources/css/app.css')
+    @vite('resources/css/app.css')
     <title>Blog</title>
-  </head>
-  <body class="mb-48">
-    @if (session()->has("message"))
+</head>
+
+<body class="mb-48">
+    @if (session()->has('message'))
         <x-flash-message>
-            <p>{{session("message")}}</p>
+            <p>{{ session('message') }}</p>
         </x-flash-message>
     @endif
     {{-- ----header--- --}}
-    @include("partials._header")
+    @include('partials._header')
     <main>
         @yield('content')
     </main>
     {{-- ----footer--- --}}
-    @include("partials._footer")
+    @include('partials._footer')
+</body>
 
-    <script src="resources/js/app.js"></script>
-  </body>
 </html>>
