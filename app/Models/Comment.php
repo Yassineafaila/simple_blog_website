d75a1f97fbca94ca9a8a624b57ef15daa85cf4ef
Yassineafaila <yassineafaila55@gmail.com>
2024-01-17 21:00:07 +0000

    add the relationship with replies and the likes

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,5 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
-    protected $fillable = ["comment", "post_id"];
+    protected $fillable = ["comment", "post_id", "user_id"];
+
+
+    //Relationship To Post
+    public function post()
+    {
+        return $this->belongsTo(Post::class, "post_id");
+    }
+
+    //Relationship To User
+    public function user()
+    {
+        return $this->belongsTo(User::class, "user_id");
+    }
+
+    //Relationship To Replies
+    public function replies(){
+        return $this->hasMany(Reply::class,"comment_Id");
+    }
+
+    //Relationship to likes
+    public function likes(){
+        return $this->hasMany(Like::class,"comment_Id");
+    }
 }
