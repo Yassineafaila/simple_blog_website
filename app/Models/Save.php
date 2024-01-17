6a812518e98c75bea7f979b7fa1012f9e0d2f8d1
Yassineafaila <yassineafaila55@gmail.com>
2024-01-17 20:54:42 +0000

    add the save model

---
+++ app/Models/Save.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Save extends Model
+{
+    use HasFactory;
+    protected $fillable=["post_id","user_id"];
+    //Relationship with user
+    public function user(){
+        return $this->belongsTo(User::class,"user_id");
+    }
+}
